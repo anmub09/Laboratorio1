ce1d31b8e7dd05786bb2d13560cae10558c88856
Andres <Andres@DESKTOP-MB8RHM6>
2021-01-17 19:55:22 -0600

    Esto es una prueba

--- src/main/java/com/mycompany/proyecto1/Test.java
+++ src/main/java/com/mycompany/proyecto1/Test.java
@@ -12,5 +12,9 @@ package com.mycompany.proyecto1;
 public class Test {
     public static void main(String[] args) {
             System.out.println("com.mycompany.proyecto1.Test.main()");
+           for (int i = 0; i < 10; i++) 
+           {
+            
+          }
+        }
     }
-}
